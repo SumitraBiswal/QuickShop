01f6b3b058879c09d485aaa19196e95e22b4a2ee
Sumitra Biswal <biswalsumi6@gmail.com>
2025-08-26 09:56:21 +0530

    update something

---
+++ src/pages/Home/Login.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+
+export default function Login() {
+const [username ,setUsername]=useState("")
+const [password,setPassword]=useState("")
+const navigate=useNavigate
+const handleLogin=(e)=>{e.preventDefault();
+    if(username==="admin"&& password==="1234"){
+        localStorage.setItem("isLoggedIn","true");
+        navigate ("/")
+    }else{
+        alert ("invalid username or Password")
+    }
+};
+  return (
+    <div className="logincontainer">
+      <form className="login box" onSubmit={handleLogin}> 
+        <h2>Login</h2> 
+        <input tyep="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e)=>setUsername(e.target.value)}
+        />
+        <input
+        type="text"
+        placeholder="Password"
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
+        />
+        <button tyep= "submit">Login</button>
+         </form>
+    </div>
+  );
+}
+
